2b876a2fe61ee22cbbfd3943d4b9c4867d78984d
fraalepal <fraalepal@alum.us.es>
2020-04-06 23:09:00 +0200

    Añado la vista show de las causas sin listado de donaciones

--- src/main/java/org/springframework/samples/petclinic/web/CauseController.java
+++ src/main/java/org/springframework/samples/petclinic/web/CauseController.java
@@ -65,4 +65,12 @@ public class CauseController {
 		return "redirect:/";
 	}
 	
+	@GetMapping("/show/{causeId}")
+	public ModelAndView showCause(@PathVariable("causeId") int causeId) {
+		ModelAndView mav = new ModelAndView("causes/causeDetails");
+		mav.addObject(this.clinicService.findCauseById(causeId));
+		return mav;
+	}
+
+	
 }
